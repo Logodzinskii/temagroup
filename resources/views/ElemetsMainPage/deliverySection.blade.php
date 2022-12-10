ef15c92bc558c08868886919a36e2e93a5a36f99
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-12-10 21:38:15 +0500

    arrow check info

---
+++ resources/views/ElemetsMainPage/deliverySection.blade.php
@@ -0,0 +1,21 @@
+
+<section class="container-fluid bg-primary text-white row d-flex justify-content-center align-items-center flex-wrap">
+    <h2 class="text-center top-bottom-50 title-section-white">ДОСТАВКА И МОНТАЖ</h2>
+    <div class="col-4">
+        <img src="{{asset("images/main/img_1.jpg")}}" alt=""/>
+    </div>
+    <div class="row d-flex justify-content-center align-items-center top-bottom-50" >
+        <div class="col-6 container fs-4 text-center d-flex align-items-center justify-content-center flex-wrap">
+            <p style="width: 100%; padding: 2vh 0 2vh 0" >
+                <span class="text-center" style="width: 50%">Идеальная геометрия <br/>  корпуса кухни</span>
+            </p>
+            <p style="width: 100%; padding: 2vh 0 2vh 0" >
+                <span class="text-center" style="width: 50%">Одинаковые зазоры <br/> между фасадами</span>
+            </p>
+            <p style="width: 100%; padding: 2vh 0 2vh 0">
+                <span class="text-center" style="width: 50%">Плавная и четкая <br/> работа всех механизмов</span>
+            </p>
+        </div>
+
+    </div>
+</section>
