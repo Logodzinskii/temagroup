b2e6a0d527d09e3e51c53d771ad0555f0dc5c3d2
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2023-01-06 08:54:29 +0500

    add cookies to all page

---
+++ resources/views/ElemetsMainPage/script.blade.php
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="icon" type="image/png" href="{{ asset('images/logo/logo.png') }}"/>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href={{ asset('css/owl.carousel.min.css') }}>
+<link rel="stylesheet" href={{ asset('css/owl.theme.default.min.css') }}>
+<link rel="stylesheet" href={{ asset('css/main.css') }}>
+<link rel="stylesheet" href={{ asset('css/bootstrap.css') }}>
+<script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+<script src={{ asset('js/owl.carousel.min.js')}}></script>
+<script src="{{asset('js/coockiInfo.js')}}"></script>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,16 +5,7 @@
         <META NAME="description" content="{{$descryptions['descryption']}}">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <title>{{$descryptions['title']}}</title>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link rel="icon" type="image/png" href="{{ asset('images/logo/logo.png') }}"/>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href={{ asset('css/owl.carousel.min.css') }}>
-        <link rel="stylesheet" href={{ asset('css/owl.theme.default.min.css') }}>
-        <link rel="stylesheet" href={{ asset('css/main.css') }}>
-        <link rel="stylesheet" href={{ asset('css/bootstrap.css') }}>
-        <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
-        <script src={{ asset('js/owl.carousel.min.js')}}></script>
-
+        @include('ElemetsMainPage/script')
         <script type="text/javascript">
         $(document).ready(function(){
 
